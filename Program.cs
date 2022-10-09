e37d324d7a2e0c19337d6031e25b7ecd96d48dc3
BerAndrade <111024132+BerAndrade@users.noreply.github.com>
2022-10-09 16:35:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+
+{
+
+    static void vetor(double[] num)
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            Console.Write("digite um numero real :");
+            num[i] = double.Parse(Console.ReadLine());
+        }
+    }
+
+    static void numero(double[] num)
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            if (num[i] % 2 == 0)
+            {
+                double par = num[i] * 2;
+                Console.WriteLine($"numero {num[i]} par = {par}");
+            }
+            else
+            {
+                double impar = num[i] + 2;
+                Console.WriteLine($"numero {num[i]} impar = {impar}");
+            }
+        }
+           
+    }
+    static void Main(string[] args)
+    {
+        double[] num = new double[10];
+        Console.WriteLine("Atividade 1 - Questão 2");
+        vetor(num);
+        numero(num);
+    }
+} 
+
